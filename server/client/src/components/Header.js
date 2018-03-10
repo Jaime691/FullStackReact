52d5ea4556c7b5fffcfac434df9df783087ada9b
Jaime <jmayorgadev@gmail.com>
2018-03-10 15:51:33 -0500

    Auth process completed

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   renderContent() {
     switch (this.props.auth) {
       case null:
-        return 'Still waiting';
+        return;
       case false:
-        return 'Logged out';
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        );
       default:
-        return 'logged in';
+        return (
+          <li>
+            <a href="/api/logout">Log out</a>
+          </li>
+        );
     }
   }
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">Emaily</a>
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
+            className="left brand-logo"
+          >
+            Emaily
+          </Link>
           <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
