850711b4972cf12a049b30d2d267f036cca8f530
Jaime <jmayorgadev@gmail.com>
2019-03-05 22:55:46 -0500

    Till lesson 101

--- server/index.js
+++ server/index.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const keys = require('./config/keys');
-require('./models/User');
-require('./services/passport');
+const express = require('express')
+const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+const keys = require('./config/keys')
+require('./models/User')
+require('./services/passport')
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI)
 
-const app = express();
+const app = express()
 
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     keys: [keys.cookieKey]
   })
-);
+)
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
-require('./routes/authRoutes')(app);
+require('./routes/authRoutes')(app)
 
-const PORT = process.env.PORT || 5000; // process.env.PORT is defined by Heroku if not then PORT is 5000
-app.listen(PORT);
+const PORT = process.env.PORT || 4000 // process.env.PORT is defined by Heroku if not then PORT is 5000
+app.listen(PORT)
